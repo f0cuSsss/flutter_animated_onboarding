2068a711d1d20adabb6dc5da3b064e61d94a6981
f0cuSsss <f0cuSmcsza@gmail.com>
2021-06-22 15:16:02 +0300

    Choose language screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_welcome_screen/behaviors/NoneScrollBehavior.dart';
 import 'package:flutter_welcome_screen/on_board_screen.dart';
 
 void main() {
   runApp(
     MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(fontFamily: 'IBM Plex Sans'),
       home: OnBoardScreen(),
     ),
   );
--- lib/on_board_screen.dart
+++ lib/on_board_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:flutter_welcome_screen/behaviors/NoneScrollBehavior.dart';
 
 class OnBoardScreen extends StatefulWidget {
   OnBoardScreen({Key? key}) : super(key: key);
@@ -20,18 +21,40 @@ class LanguageItem {
 class _OnBoardScreenState extends State<OnBoardScreen> {
   final pageController = new PageController();
 
+  LanguageItem selectedLanguage = new LanguageItem(id: -1, name: '', path: '');
   final languages = [
-    LanguageItem(id: 1, name: 'English', path: 'assets/onboarding/us.svg'),
-    LanguageItem(id: 2, name: 'German', path: 'assets/onboarding/germany.svg'),
-    LanguageItem(id: 3, name: 'Russian', path: 'assets/onboarding/russia.svg'),
     LanguageItem(
-        id: 4, name: 'Ukrainian', path: 'assets/onboarding/ukraine.svg'),
+      id: 1,
+      name: 'English',
+      path: 'assets/onboarding/us.svg',
+    ),
+    LanguageItem(
+      id: 2,
+      name: 'German',
+      path: 'assets/onboarding/germany.svg',
+    ),
+    LanguageItem(
+      id: 3,
+      name: 'Russian',
+      path: 'assets/onboarding/russia.svg',
+    ),
+    LanguageItem(
+      id: 4,
+      name: 'Ukrainian',
+      path: 'assets/onboarding/ukraine.svg',
+    ),
   ];
-  final int selectedLanguage = -1;
+
+  @override
+  void initState() {
+    super.initState();
+    selectedLanguage = languages[0];
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xFFEFF3F8),
       body: SafeArea(
         child: Container(
           child: PageView(
@@ -52,117 +75,152 @@ class _OnBoardScreenState extends State<OnBoardScreen> {
 
   Widget _renderLanguagePage() {
     return Container(
-      child: Column(
-        children: [
-          // Text(
-          //   'Welcome!',
-          //   style: TextStyle(
-          //     color: Color(0xFF8093F1),
-          //     // color: Color(0xFFB388EB),
-          //     fontSize: 34,
-          //     fontWeight: FontWeight.w800,
-          //     height: 2,
-          //     letterSpacing: 0.5,
-          //   ),
-          // ),
-          Image.asset('assets/onboarding/language_globe.png'),
-          // Text(
-          //   'Pick a language to get started',
-          //   style: TextStyle(
-          //     color: Color(0xFF8093F1),
-          //     fontSize: 20,
-          //     fontWeight: FontWeight.w800,
-          //     height: 1.5,
-          //     letterSpacing: 0.5,
-          //   ),
-          // ),
-          Expanded(
-            child: Column(
+      child: Padding(
+        padding:
+            const EdgeInsets.only(bottom: 20, top: 70, left: 30, right: 30),
+        // bottom: MediaQuery.of(context).size.height * 0.1),
+        child: Column(
+          children: [
+            Text(
+              'Choose a language, please!',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Color(0xFF03002D),
+                fontFamily: 'Roboto',
+                fontSize: 36,
+                fontWeight: FontWeight.w900,
+                height: 1.5,
+                letterSpacing: 0.5,
+              ),
+            ),
+            SizedBox(height: 40),
+            Expanded(
+              child: ScrollConfiguration(
+                behavior: NoneScrollBehavior(),
+                child: ListView(
+                  children: List.generate(
+                    languages.length,
+                    (index) => _languageItem(languages[index]),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 30),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                // SizedBox(height: 40),
-                Row(
-                  children: [
-                    SizedBox(width: 20),
-                    _languageItem(languages[0]),
-                    SizedBox(width: 15),
-                    _languageItem(languages[1]),
-                    SizedBox(width: 20),
-                  ],
+                // NEED TO REFACTOR!!!
+                Expanded(
+                  child: IconButton(
+                    iconSize: 30,
+                    splashColor: (() {
+                      return pageController.initialPage == 0
+                          ? Colors.transparent
+                          : Color(0xFF8093F1);
+                    }()),
+                    highlightColor: Colors.transparent,
+                    color: (() {
+                      return pageController.initialPage == 0
+                          ? Colors.transparent
+                          : Color(0xFF8093F1);
+                    }()),
+                    icon: Icon(Icons.arrow_back),
+                    onPressed: () => print('Prev slide'),
+                  ),
                 ),
-                SizedBox(height: 20),
-                Row(
-                  children: [
-                    SizedBox(width: 20),
-                    _languageItem(languages[2]),
-                    SizedBox(width: 15),
-                    _languageItem(languages[3]),
-                    SizedBox(width: 20),
-                  ],
+                Expanded(child: Text('')),
+                Expanded(
+                  child: Ink(
+                    decoration: ShapeDecoration(
+                      shape: CircleBorder(),
+                      color: Color(0xFF6338F2),
+                    ),
+                    padding: EdgeInsets.all(10),
+                    child: IconButton(
+                      iconSize: 24,
+                      color: Color(0xFF8093F1),
+                      icon: Icon(
+                        Icons.arrow_forward,
+                        color: Colors.white,
+                      ),
+                      onPressed: selectedLanguage.id != -1
+                          ? () {
+                              pageController.nextPage(
+                                duration: const Duration(milliseconds: 200),
+                                curve: Curves.ease,
+                              );
+                            }
+                          : null,
+                    ),
+                  ),
                 ),
-                SizedBox(height: 20),
               ],
             ),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Expanded(
-                child: IconButton(
-                  iconSize: 30,
-                  color: Color(0xFF8093F1),
-                  icon: Icon(Icons.arrow_back),
-                  onPressed: () => print('Prev slide'),
-                ),
-              ),
-              Expanded(child: Text('')),
-              Expanded(
-                child: IconButton(
-                  iconSize: 30,
-                  disabledColor: Colors.red,
-                  color: Color(0xFF8093F1),
-                  icon: Icon(Icons.arrow_forward),
-                  onPressed: () {
-                    pageController.nextPage(
-                      duration: const Duration(milliseconds: 200),
-                      curve: Curves.ease,
-                    );
-                  },
-                ),
-              ),
-            ],
-          ),
-          SizedBox(height: 30),
-        ],
+            SizedBox(height: 30),
+          ],
+        ),
       ),
     );
   }
 
   Widget _languageItem(LanguageItem item) {
-    return Expanded(
-      child: Container(
-        decoration: BoxDecoration(
-          color: Color(0xFF8093F1).withOpacity(0.15),
-          borderRadius: BorderRadius.all(Radius.circular(12)),
-        ),
-        padding: EdgeInsets.all(10),
-        child: Column(
-          children: [
-            SizedBox(height: 5),
-            SvgPicture.asset(
-              item.path,
-              height: 45,
-              width: 40,
-            ),
-            SizedBox(height: 10),
-            Text(
-              item.name,
-              style: TextStyle(
-                fontSize: 15,
-                color: Colors.black.withOpacity(0.5),
-                fontWeight: FontWeight.w400,
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          selectedLanguage = item;
+        });
+      },
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 12),
+        child: Container(
+          decoration: BoxDecoration(
+            color: selectedLanguage.id == item.id
+                ? Color(0xFF03002D)
+                : Colors.white,
+            borderRadius: BorderRadius.all(Radius.circular(12)),
+          ),
+          width: double.infinity,
+          padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 18),
+          child: Row(
+            children: [
+              SvgPicture.asset(
+                item.path,
+                height: 36,
+                width: 36,
               ),
-            ),
-          ],
+              const SizedBox(width: 35),
+              Expanded(
+                child: Text(
+                  item.name,
+                  style: TextStyle(
+                    fontSize: 19,
+                    color: selectedLanguage.id == item.id
+                        ? Colors.white
+                        : Colors.black,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.all(5),
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  border: Border.all(
+                    width: 1,
+                    color: Colors.grey.withOpacity(0.4),
+                  ),
+                  color:
+                      selectedLanguage.id == item.id ? Color(0xFF6338F2) : null,
+                ),
+                child: Icon(
+                  Icons.done,
+                  color: selectedLanguage.id == item.id
+                      ? Colors.white
+                      : Colors.grey.withOpacity(0.4),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
